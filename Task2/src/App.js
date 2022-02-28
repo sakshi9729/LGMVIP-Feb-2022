e41f6b5f4a647033df96f026e5eaea90bab88800
sakshi9729 <54631908+sakshi9729@users.noreply.github.com>
2022-02-28 22:48:55 +0530

    Add files via upload

---
+++ Task2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GetUserData from "./components/userData";
+
+function App() {
+  return (
+    <div className="App">
+      <GetUserData />
+    </div>
+  );
+}
+
+export default App;
